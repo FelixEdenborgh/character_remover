42a5b89119d4d6768b8b5f0a9003c1279f9792d4
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:16:16 +0200

    Add files via upload

---
+++ character_Remover/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace character_Remover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string changer;
+            Console.WriteLine("Enter an world: ");
+            changer = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Time to remove characters in the string ;)");
+            
+        }
+    }
+}
